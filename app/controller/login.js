269184d12a396692872d270b0a83aa274f718df5
sqy <1186386839@qq.com>
2021-10-09 09:11:39 +0800

    'bug修复'

--- app/controller/login.js
+++ app/controller/login.js
@@ -3,9 +3,27 @@
 const Controller = require("egg").Controller;
 
 class LoginController extends Controller {
+  constructor(props) {
+    super(props);
+    this.UserCreateRule = {
+      username: {
+        type: "string",
+        required: true,
+        allowEmpty: false,
+        format: /^[a-zA-Z0-9_-]{4,16}$/,
+      },
+      password: {
+        type: "password",
+        required: true,
+        allowEmpty: false,
+        min: 6,
+      },
+    };
+  }
   async index() {
     const { ctx, service } = this;
     const { username, password } = ctx.request.body;
+    ctx.validate(this.UserCreateRule);
     await service.login.login({ username, password });
   }
 }
